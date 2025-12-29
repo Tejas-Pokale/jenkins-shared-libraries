8dc75885add2176eda4b60df0e37d164a57a487e
Tejas Pokale <137619310+Tejas-Pokale@users.noreply.github.com>
2025-12-30 00:11:52 +0530

    Add SonarQube analysis function in Groovy

---
+++ vars/sonarqube_analysis.groovy
@@ -0,0 +1,12 @@
+def call(String sonarServer, String projectKey, String projectName) {
+    def scannerHome = tool 'SonarScanner'
+    withSonarQubeEnv(sonarServer) {
+        sh """
+            ${scannerHome}/bin/sonar-scanner \
+              -Dsonar.projectKey=${projectKey} \
+              -Dsonar.projectName=${projectName} \
+              -Dsonar.sources=.
+        """
+    }
+}
+
