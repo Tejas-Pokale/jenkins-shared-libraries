53ebe0efc3d992b13bc607763e1791f64f4ba500
Tejas Pokale <137619310+Tejas-Pokale@users.noreply.github.com>
2025-12-30 00:38:02 +0530

    Enhance formatting and echo statements in docker_build
    Refactor Docker build script for improved readability.

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,12 +1,19 @@
 def call(Map config = [:]) {
-    def imageName = config.imageName ?: error("Image name is required")
-    def imageTag = config.imageTag ?: 'latest'
+    def imageName  = config.imageName ?: error("Image name is required")
+    def imageTag   = config.imageTag ?: 'latest'
     def dockerfile = config.dockerfile ?: 'Dockerfile'
-    def context = config.context ?: '.'
-    
-    echo "Building Docker image: ${imageName}:${imageTag} using ${dockerfile}"
-    
+    def context    = config.context ?: '.'
+
+    echo "Building Docker image: ${imageName}:${imageTag}"
+    echo "Dockerfile: ${dockerfile}"
+    echo "Context: ${context}"
+
     sh """
-        docker build -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}
+        docker build \
+          -t ${imageName}:${imageTag} \
+          -t ${imageName}:latest \
+          -f ${dockerfile} \
+          ${context}
     """
 }
+
